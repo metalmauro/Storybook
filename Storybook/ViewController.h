555df981b1e1977bda8ec1b0fd224846fe3907b7
Matt Mauro <matt.mauro@gmail.com>
2016-11-18 14:28:16 -0500

    Initial Commit

---
+++ Storybook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Storybook
+//
+//  Created by Matthew Mauro on 2016-11-18.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Storybook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Storybook
+//
+//  Created by Matthew Mauro on 2016-11-18.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
